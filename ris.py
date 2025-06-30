409ff5cfdeb8f3ae3a04fa3e817bb49b00e91ad9
Hayden Schroeder <hschroeder0317@gmail.com>
2025-06-30 13:35:45 -0500

    Fully connected model

--- ris.py
+++ ris.py
@@ -9,6 +9,15 @@ import pandas as pd
 import numpy as np
 from keras.utils import to_categorical
 import datetime
+from keras.models import Sequential
+from keras.layers import Lambda, Dense, Flatten, Input, Dropout
+from keras.callbacks import EarlyStopping
+from keras.layers import BatchNormalization, Convolution2D, MaxPooling2D
+from keras.optimizers import RMSprop, Adam
+from keras.src.legacy.preprocessing.image import ImageDataGenerator  # ImageDataGenerator has been deprecated in keras
+from sklearn.model_selection import train_test_split
+
+BATCH_SIZE = 64
 
 
 def load_data():
@@ -38,15 +47,16 @@ seed = 33
 np.random.seed(seed)
 
 
-def linear_model(x_train, y_train):
-    from keras.models import Sequential
-    from keras.layers import Lambda, Dense, Flatten, Input, Dropout
-    from keras.callbacks import EarlyStopping
-    from keras.layers import BatchNormalization, Convolution2D, MaxPooling2D
-    from keras.optimizers import RMSprop
-    from keras.src.legacy.preprocessing.image import ImageDataGenerator  # ImageDataGenerator has been deprecated in keras
-    from sklearn.model_selection import train_test_split
+def generate_batches(x, y, batch_size=64):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=seed)
+    generator = ImageDataGenerator()
+    batches = generator.flow(x_train, y_train, batch_size=batch_size)
+    test_batches = generator.flow(x_test, y_test, batch_size=batch_size)
+    return batches, test_batches
 
+
+def linear_model(x_train, y_train):
+    """ Train a simple linear model on the MNIST dataset."""
     BATCH_SIZE = 64
 
     model = Sequential()
@@ -56,19 +66,36 @@ def linear_model(x_train, y_train):
     model.add(Dense(10, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer=RMSprop(), metrics=['accuracy'])
-    generator = ImageDataGenerator()
 
-    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=seed)
-
-    # Create shuffled batches to reduce memory usage
-    batches = generator.flow(x_train, y_train, batch_size=BATCH_SIZE)
-    test_batches = generator.flow(x_test, y_test, batch_size=BATCH_SIZE)
+    batches, test_batches = generate_batches(x_train, y_train, BATCH_SIZE)
 
     model.fit(batches, epochs=4, validation_data=test_batches,
               steps_per_epoch=batches.n, validation_steps=test_batches.n)
 
     # Save the model
     model.save(f'linear-model-{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")}.keras')
+
+
+def fully_connected_model(x_train, y_train):
+    """ Train a fully connected model on the MNIST dataset."""
+    BATCH_SIZE = 64
+
+    model = Sequential()
+    model.add(Input(shape=(28, 28, 1)))
+    model.add(Lambda(standardize))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.01), metrics=['accuracy'])
+
+    batches, test_batches = generate_batches(x_train, y_train, BATCH_SIZE)
+
+    model.fit(batches, epochs=4, validation_data=test_batches,
+              steps_per_epoch=batches.n, validation_steps=test_batches.n)
+
+    # Save the model
+    model.save(f'fully-connected-model-{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")}.keras')
 
 
 x_train, y_train, x_test = load_data()
@@ -85,4 +112,5 @@ def standardize(x):
 # One hot encoding of labels
 y_train = to_categorical(y_train, num_classes=10)
 
-linear_model(x_train, y_train)
+# linear_model(x_train, y_train)
+fully_connected_model(x_train, y_train)
